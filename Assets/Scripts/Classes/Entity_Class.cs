5c405da3397c17e9d95ca45c4621803b69994b2e
Denise Franz <denisealiciafranz@gmail.com>
2025-07-07 14:08:51 +0200

    Entity Class test framework

--- Assets/Scripts/Classes/Entity_Class.cs
+++ Assets/Scripts/Classes/Entity_Class.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Entity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
