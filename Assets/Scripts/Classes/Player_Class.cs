29a1f0e295b9520fd9bb66678b7f1273aa39b040
Denise <denisealiciafranz@gmail.com>
2025-07-16 11:22:45 +0200

    Player class

---
+++ Assets/Scripts/Classes/Player_Class.cs
@@ -0,0 +1,10 @@
+namespace Diceonomicon
+
+using UnityEngine;
+
+public class Player : Entity
+{
+    public Impling[] ImplingRoster;
+    public Impling[] ActiveImplings;
+    public int MaxImplings;
+}
