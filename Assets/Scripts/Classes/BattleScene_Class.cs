f941e14ef57187d20d45fdf5a6a962d0ccdc9efd
Denise <denisealiciafranz@gmail.com>
2025-07-17 10:03:21 +0200

    combat manager first function implementation

--- Assets/Scripts/Classes/BattleScene_Class.cs
+++ Assets/Scripts/Classes/BattleScene_Class.cs
@@ -1,37 +1,37 @@
 namespace Diceonomicon
 {
-using UnityEngine;
+    using UnityEngine;
 
-public class BattleScene : MonoBehaviour 
-{
-    public Player Player;
-     public Enemy[] Enemies;
+    public class BattleScene
+    {
+        public Player Player;
+        public Enemy[] Enemies;
         public Vector3[] PlayerPositions;
         public Vector3[] EnemyPositions;
-    public DiceSlot[] PlayerActiveColumn;
-    public DiceSlot[] EnemyActiveColumn;
-    public int Level;
+        public DiceSlot[] PlayerActiveColumn;
+        public DiceSlot[] EnemyActiveColumn;
+        public int Level;
 
-    public void BuildScene()
-    {
-        Debug.Log("BattleScene.BuildScene");
-    }
-    public void PlacementPhase()
-    {
-        Debug.Log("BattleScene.PlacementPhase");
-    }
-    public void CalculateDamage()
-    {
-        Debug.Log("BattleScene.CalculateDamage");
-    }
-    public void EndOfRound()
-    {
-        Debug.Log("BattleScene.EndOfRound");
-    }
+        public void BuildScene()
+        {
+            Debug.Log("BattleScene.BuildScene");
+        }
+        public void PlacementPhase()
+        {
+            Debug.Log("BattleScene.PlacementPhase");
+        }
+        public void CalculateDamage()
+        {
+            Debug.Log("BattleScene.CalculateDamage");
+        }
+        public void EndOfRound()
+        {
+            Debug.Log("BattleScene.EndOfRound");
+        }
         private void ResetEntity(Entity _Entity)
-    {
-        Debug.Log("BattleScene.Reset");
-    }
+        {
+            Debug.Log("BattleScene.Reset");
+        }
 
     }
 }
--- Assets/Scripts/Classes/CombatManager_Class.cs
+++ Assets/Scripts/Classes/CombatManager_Class.cs
@@ -1,24 +1,40 @@
 namespace Diceonomicon
 {
-using UnityEngine;
+    using UnityEngine;
 
-public static class CombatManager : MonoBehaviour
-{
-    private DiceSlot[] PlayerColumn;
-    private DiceSlot[] EnemyColumn;
+    public static class CombatManager
+    {
+        private DiceSlot[] PlayerSlots;
+        private DiceSlot[] EnemySlots;
 
-    public void HandleActiveCombat() {
-        Debug.Log("CombatManager.HandleActiveCombat");
-    }
+        public static void HandleActiveCombat(BattleScene _BattleScene)
+        {
+            Debug.Log("CombatManager.HandleActiveCombat");
+
+            this.PlayerSlots = SortActiveSlots(_BattleScene.PlayerActiveColumn);
+            this.EnemySlots = SortActiveSlots(_BattleScene.EnemyActiveColumn);
+
+            for (int i = 0; i < PlayerSlots.length; i++)
+            {
+                HandleSlotEffects(PlayerSlots[i]);
+                HandleSlotEffects(EnemySlots[i]);
+
+            }
+        }
 
         private static void HandleSlotEffects(DiceSlot[] _ActiveSlots)
         {
-        Debug.Log("CombatManager.HandleSlotEffects");
-    }
+            Debug.Log("CombatManager.HandleSlotEffects");
+
+        foreach (DiceSlot Slot in _ActiveSlots)
+            {
+                Slot.DoEffect();
+            }
+        }
 
         private static DiceSlot[][] SortActiveSlots(DiceSlot[] _ActiveColumn) {
-        Debug.Log("CombatManager.SortActiveSlots");
-    }
+            Debug.Log("CombatManager.SortActiveSlots");
+        }
 
     }
 }
