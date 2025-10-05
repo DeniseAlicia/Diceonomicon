bb9b7991a7f9465056ab3babe21decbbb72ba7c5
MatBudimir <dotbudi@gmail.com>
2025-10-05 11:53:13 +0200

    candle health ui

--- Assets/Scripts/Classes/Entity.cs
+++ Assets/Scripts/Classes/Entity.cs
@@ -9,8 +9,7 @@ public abstract class Entity : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
-    public Material healthMaterial;
-    public Material overlayMaterial;
+    public GameObject candle;
     public TMP_Text healthText;
     public TMP_Text damageText;
      public TMP_Text blockText;
@@ -36,28 +35,7 @@ public abstract class Entity : MonoBehaviour
 
     public void Update()
     {
-        // Add depleting health bar effect
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-
-        float yOffset = 0.5f - currentHealth * 0.2f / maxHealth;
-        Vector4 healthOffset = healthMaterial.GetVector("_TopMap_ST");
-        healthOffset.w = yOffset;
-        healthMaterial.SetVector("_TopMap_ST", healthOffset);
-
-        // Adjusting Alpha of Health UI elements
-        alpha = Mathf.Clamp(alpha, 0f, 1f);
-
-        Color healthColor = healthMaterial.GetColor("_BaseColor");
-        healthColor.a = alpha / 4;
-        healthMaterial.SetColor("_BaseColor", healthColor);
-
-        Color overlayColor = overlayMaterial.color;
-        overlayColor.a = alpha * 2;
-        overlayMaterial.color = overlayColor;
-
-        Color textColor = healthText.color;
-        textColor.a = alpha;
-        healthText.color = textColor;
+        
     }
 
     public void DrawDice()
