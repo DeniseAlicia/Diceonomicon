36a7e68b1553b15c9418a7376784dd5504633d4a
Flo <florine.bienhuels@gmail.com>
2025-10-20 19:53:28 +0200

    newMenu

---
+++ Assets/Scripts/Menu/Impling_Selection.cs
@@ -0,0 +1,20 @@
+using Mono.Cecil.Cil;
+using UnityEngine;
+
+public class Impling_Selection : MonoBehaviour
+{
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
